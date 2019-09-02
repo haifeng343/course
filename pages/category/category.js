9faa8dfd5163b1358e6a926287d529b443a4939a
fenghaifeng <1784542447@qq.com>
2019-09-02 19:01:07 +0800

    upload

---
+++ pages/category/category.js
@@ -0,0 +1,19 @@
+
+Page({
+
+  data: {
+
+  },
+  onShow: function () {
+
+  },
+  changeType:function() {
+    console.log(1111)
+  },
+  onReachBottom: function () {
+
+  },
+  onShareAppMessage: function () {
+
+  }
+})
